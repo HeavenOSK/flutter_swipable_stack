e41784f58b0ff4ea23f3e51864939fe116909c01
HeavenOSK <heavenosk@gmail.com>
2021-12-28 07:41:32 +0900

    add test for #34

--- test/swipable_stack_test.dart
+++ test/swipable_stack_test.dart
@@ -1,5 +1,56 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:swipable_stack/src/swipable_stack.dart';
+
+final _cardColors = <Color>[
+  ...Colors.primaries,
+  ...Colors.accents,
+];
+
+Widget _buildCard({
+  required Color color,
+  required int index,
+}) =>
+    Container(
+      key: UniqueKey(),
+      height: double.infinity,
+      width: double.infinity,
+      color: color,
+      alignment: Alignment.center,
+      child: Text('$index'),
+    );
+
+Widget _buildApp({
+  required int cardCount,
+}) {
+  return MaterialApp(
+    home: Scaffold(
+      body: SwipableStack(
+        itemCount: cardCount,
+        builder: (context, index, constraints) {
+          final color = _cardColors[index % _cardColors.length];
+          return _buildCard(
+            color: color,
+            index: index,
+          );
+        },
+      ),
+    ),
+  );
+}
 
 void main() {
-  test('', () {});
+  group(
+    '[SwipableStack]',
+    () {
+      testWidgets(
+        'can build when itemCount is 0, 1, 2, 3...10',
+        (tester) async {
+          for (var i = 0; i < 10; i++) {
+            await tester.pumpWidget(_buildApp(cardCount: i));
+          }
+        },
+      );
+    },
+  );
 }
